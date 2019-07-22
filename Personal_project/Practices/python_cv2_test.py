bc8c070f8df8c18883cab94d9eb1e33108ce7c9d
LukeSungukJung <xrjseka615@gmail.com>
2019-07-22 12:43:17 +0900

    nlp start

--- Personal_project/Practices/python_cv2_test.py
+++ Personal_project/Practices/python_cv2_test.py
@@ -1,6 +1,12 @@
 import cv2
 from matplotlib import pyplot as plt 
+from keras.layers import *
+from keras.models import *
+
+
 
 cvimg = cv2.imread('cv_test.jpg')
 
-pl_img = plt.imread('cv_test.jpg')
+cv_img_rgb= cv2.cvtColor(cvimg,cv2.COLOR_BGR2RGB)
+
+cv_img_canny = cv2.Canny(cv_img_rgb[:,:,2],100,200)
